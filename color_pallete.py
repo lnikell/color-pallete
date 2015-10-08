efef7cb417b3a69e0c9822051b38c3519a68b371
Alexey Barashkov <lnikell@gmail.com>
2015-10-08 23:08:12 +0500

    first commit

---
+++ color_pallete.py
@@ -0,0 +1,27 @@
+import sublime, sublime_plugin
+import json
+import os
+
+class ColorPallete(sublime_plugin.TextCommand):
+  def run(self, edit):
+    print(sublime.active_window().extract_variables())
+    variables = sublime.active_window().extract_variables()
+    pallete_path = os.path.join(variables['folder'], "color_pallete.json")
+    with open(pallete_path) as data_file:    
+      pallete_file = json.load(data_file)
+    print(pallete_file)
+    pallete = pallete_file["pallete"]
+    out = ""
+    for color in pallete:
+      out += '<style>a {font-size:30px}</style><a style="color: %s" href="%s">■</a>&nbsp;' %(color, color)
+    self.view.show_popup(out, on_navigate=self.onClick)
+
+  def onClick(self, color):
+    self.view.run_command("insert_color_data", {"args":{'color':color}})
+
+class InsertColorData(sublime_plugin.TextCommand):
+  def run(self, edit, args):
+    self.view.insert(edit, self.view.sel()[0].begin(), args['color'])
+
+
+
